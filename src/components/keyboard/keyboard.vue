ff51b675614e8aa5fc1a36681ac4a23086f5aaf6
panth <panth.solanki@livspace.com>
2021-05-04 09:38:45 +0530

    keyboard code restructured

--- src/components/keyboard/keyboard.vue
+++ src/components/keyboard/keyboard.vue
@@ -1,13 +1,19 @@
-<template>
-  <div>keyboard</div>
-</template>
+<template src="./keyboard.html"></template>
+<style scoped lang="scss" src="./keyboard.scss"></style>
 
 <script>
+import alphabets from "@/constant";
 export default {
-
-}
+  data() {
+    return {
+      alphabetsInput: alphabets,
+      isCapsLockPressed: true
+    };
+  },
+  methods: {
+    getActiveValue(input) {
+      return this.isCapsLockPressed ? input.shiftValue : input.value;
+    }
+  }
+};
 </script>
-
-<style>
-
-</style>
