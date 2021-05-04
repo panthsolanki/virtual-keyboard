01cb3ca01cfbe48a67ab4093d417976c4dc564a3
panth <panth.solanki@livspace.com>
2021-05-04 09:28:38 +0530

    constants file added

---
+++ src/constant.js
@@ -0,0 +1,133 @@
+const alphabets = [
+  {
+    value: "a",
+    shiftValue: "A",
+    type: "alphabet"
+  },
+  {
+    value: "b",
+    shiftValue: "B",
+    type: "alphabet"
+  },
+  {
+    value: "c",
+    shiftValue: "C",
+    type: "alphabet"
+  },
+  {
+    value: "d",
+    shiftValue: "D",
+    type: "alphabet"
+  },
+  {
+    value: "e",
+    shiftValue: "E",
+    type: "alphabet"
+  },
+  {
+    value: "f",
+    shiftValue: "F",
+    type: "alphabet"
+  },
+  {
+    value: "g",
+    shiftValue: "G",
+    type: "alphabet"
+  },
+  {
+    value: "h",
+    shiftValue: "H",
+    type: "alphabet"
+  },
+  {
+    value: "i",
+    shiftValue: "I",
+    type: "alphabet"
+  },
+  {
+    value: "j",
+    shiftValue: "J",
+    type: "alphabet"
+  },{
+    value: "k",
+    shiftValue: "K",
+    type: "alphabet"
+  },
+  {
+    value: "l",
+    shiftValue: "L",
+    type: "alphabet"
+  },
+  {
+    value: "m",
+    shiftValue: "M",
+    type: "alphabet"
+  },
+  {
+    value: "n",
+    shiftValue: "N",
+    type: "alphabet"
+  },
+  {
+    value: "o",
+    shiftValue: "O",
+    type: "alphabet"
+  },
+  {
+    value: "p",
+    shiftValue: "P",
+    type: "alphabet"
+  },
+  {
+    value: "q",
+    shiftValue: "Q",
+    type: "alphabet"
+  },
+  {
+    value: "r",
+    shiftValue: "R",
+    type: "alphabet"
+  },
+  {
+    value: "s",
+    shiftValue: "S",
+    type: "alphabet"
+  },
+  {
+    value: "t",
+    shiftValue: "T",
+    type: "alphabet"
+  },
+  {
+    value: "u",
+    shiftValue: "U",
+    type: "alphabet"
+  },
+  {
+    value: "v",
+    shiftValue: "V",
+    type: "alphabet"
+  },
+  {
+    value: "w",
+    shiftValue: "W",
+    type: "alphabet"
+  },
+  {
+    value: "x",
+    shiftValue: "X",
+    type: "alphabet"
+  },
+  {
+    value: "y",
+    shiftValue: "Y",
+    type: "alphabet"
+  },
+  {
+    value: "z",
+    shiftValue: "Z",
+    type: "alphabet"
+  }
+];
+
+export default alphabets;
